7c0e9b29f51eecfb87e2d3f56ed33c46e97cbe7f
luckyDTF <1157617972@qq.com>
2018-04-12 13:50:45 +0800

    添加商品设置代码

---
+++ src/com/company/vo/CommodityQueryvo.java
@@ -0,0 +1,28 @@
+package com.company.vo;
+
+public class CommodityQueryvo {
+
+    private String txtname;
+
+    public String getTxtname() {
+        return txtname;
+    }
+
+    public void setTxtname(String txtname) {
+        this.txtname = txtname;
+    }
+
+    public String getCommodityTypeID() {
+        return commodityTypeID;
+    }
+
+    public void setCommodityTypeID(String commodityTypeID) {
+        this.commodityTypeID = commodityTypeID;
+    }
+
+    private String commodityTypeID;
+
+//    private Page<Commodity> page;
+
+
+}
